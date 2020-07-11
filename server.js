8e97c56747c288bef154bc4e4ebb3f7cd81bf34a
luka <lukajosegamulin@outlook.com>
2020-07-12 00:52:34 +1000

    adding changes to server for prod

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
-
+const path = require('path');
 //Connect DB 
 connectDB();
 
 //middleware
 app.use(express.json({extended:false}));
 
-
-app.get('/', (req,res)=> {
-    res.send({msg:'Welcome to the contact keeper API!'});
-})
-
 app.use('/api/users',require('./routes/users'))
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/contacts',require('./routes/contacts'))
+
+if(process.env.NODE_ENV==='production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=> {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(PORT,() => console.log('Server is running on port:',PORT));
